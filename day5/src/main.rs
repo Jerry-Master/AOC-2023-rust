30fa7463f6a10a6ef1856718d8d30eaf9d655a41
Jerry-Master <joseperez2000@hotmail.es>
2023-12-05 13:45:21 +0100

    part2 linear

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use clap::Parser;
-use std::cmp::{Eq, PartialEq, Ord, PartialOrd};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None, help_template = "\
@@ -17,181 +16,75 @@ struct Args {
     input_path: String,
 }
 
-#[derive(Eq, PartialEq, Ord, PartialOrd, Default)]
-#[derive(Debug)]
-#[derive(Clone, Copy)]
-struct Range {
-    src_start: u64,
-    src_end: u64,
-    dst_start: u64
-}
-
-impl Range {
-    fn has_in(&self, val: u64) -> bool {
-        return self.src_start <= val && val <= self.src_end;
-    }
-
-    fn has_left(&self, val: u64) -> bool {
-        return self.src_start > val;
-    }
-
-    fn has_right(&self, val: u64) -> bool {
-        return val > self.src_end;
-    }
-
-    fn map(&self, val: u64) -> u64 {
-        return self.dst_start + (val - self.src_start);
-    }
-}
-
-#[derive(Debug)]
-struct Map {
-    ranges: Vec<Range>
-}
-
-
-impl Map {
-    fn new() -> Self {
-        return Map { ranges: Vec::<Range>::new() };
-    }
-
-    fn _push(&mut self, val: Range) {
-        self.ranges.push(val);
-    }
-
-    fn push(&mut self, val: &str) {
-        let range_values: Vec<u64> = val
-                            .split_whitespace()
-                            .map(|x| x.parse::<u64>().unwrap())
-                            .collect();
-        self._push(Range { 
-            dst_start: range_values[1],
-            src_end: range_values[0] + range_values[2] - 1,
-            src_start: range_values[0]
-        });
-    }
-
-    fn sort(&mut self) {
-        self.ranges.sort_unstable();
-    }
-
-    fn len(&self) -> usize {
-        return self.ranges.len()
-    }
-
-    fn find_interval(&self, val: u64) -> Range {
-        let mut l = 0;
-        let mut r = self.len();
-        while r > l {
-            let m = (l + r) / 2;
-            if self.ranges[m].has_in(val) {
-                return self.ranges[m];
-            } else if self.ranges[m].has_left(val) {
-                r = m;
-            } else if self.ranges[m].has_right(val) {
-                l = m+1;
-            } else {
-                panic!();
-            }
-        }
-        return Range{
-            src_start: val,
-            src_end: val,
-            dst_start: val
-        };
-    }
-
-    fn map(&self, val: u64) -> u64 {
-        return self.find_interval(val).map(val);
-    }
-}
-
-
+// https://github.com/CantTouchDis/AoC2023/blob/master/day-05/src/bin/part2.rs
 fn main() -> io::Result<()>{
     let args = Args::parse();
     let file = File::open(args.input_path)?;
     let reader = BufReader::new(file);
+    let mut lines = reader.lines().map(|x| x.unwrap());
+    let seeds : Vec<u64> = lines.next().unwrap().split_at(6).1.split_ascii_whitespace().map(|s| s.parse::<u64>().unwrap()).collect();
+    let mut i = 0;
+    let mut ranges = vec![];
+    while i < seeds.len() {
+        ranges.push((seeds[i], seeds[i]+seeds[i+1]));
+        i += 2;
+    }
+    // skip empty line
+    lines.next();
+    let mut previous : Vec<(u64, u64)> = ranges.clone();
+    let mut current : Vec<(u64, u64)> = vec![];
+    for l in lines {
+        if l.is_empty() {
 
-    let mut seeds = Map::new();
-    let mut soil2seed = Map::new();
-    let mut fert2soil = Map::new();
-    let mut water2fert = Map::new();
-    let mut light2water = Map::new();
-    let mut temp2light = Map::new();
-    let mut hum2temp = Map::new();
-    let mut loc2hum = Map::new();
-    let mappings = [
-        &mut soil2seed, &mut fert2soil, &mut water2fert,
-        &mut light2water, &mut temp2light, &mut hum2temp,
-        &mut loc2hum
-    ];
-    let mut idx = 0;
-    // Read
-    for line in reader.lines() {
-        let line = line?;
-        if seeds.len() == 0 {
-            let seed_ranges: Vec<u64> = line
-                    .split(": ")
-                    .nth(1).unwrap()
-                    .split_whitespace()
-                    .map(|x| x.parse::<u64>().unwrap())
-                    .collect();
-            for i in 0..seed_ranges.len()/2 {
-                seeds._push(Range{
-                    src_start: seed_ranges[2*i+0],
-                    src_end: seed_ranges[2*i+0] + seed_ranges[2*i+1] - 1,
-                    dst_start: 0
-                });
+        }
+        else if !l.chars().nth(0).unwrap().is_digit(10) {
+            // start a new map
+            previous.append(&mut current);
+            current = vec![];
+            //println!("At start of {} we got {:?}", l, previous);
+        }
+        else {
+            let m = l.split_ascii_whitespace().map(|a| a.parse::<u64>().unwrap() ).collect::<Vec<u64>>();
+            let mut new_previous: Vec<(u64, u64)> = vec![];
+
+            // aliases to keep my sanity
+            let (dst, src_start, src_end, map_length) = (m[0], m[1], m[1] + m[2], m[2]);
+
+            for (b, e) in &previous {
+                // end before begin or begin after end
+                if *e <= src_start || *b >= src_end {
+                    new_previous.push((*b, *e));
+                    continue;
+                }
+                // full overlap
+                if *b >= src_start && *e <= src_end {
+                    current.push((dst + *b - src_start, dst + *e - src_start));
+                }
+                // fully contained
+                else if *b < src_start && *e > src_end {
+                    new_previous.push((*b, src_start));
+                    new_previous.push((src_end, *e));
+                    current.push((dst, dst + map_length));
+                }
+                // begin before begin
+                else if *b < src_start {
+                    new_previous.push((*b, src_start));
+                    current.push((dst, dst + *e - src_start));
+                }
+                else if *e > src_end {
+                    new_previous.push((src_end, *e));
+                    current.push((dst + *b - src_start, dst + map_length));
+                }
+                else {
+                    println!("{} {} {} unhandled for range ({}, {})", m[0], m[1], m[2], *b, *e);
+                }
             }
-        } else if !line.is_empty() {
-            if line.chars().nth(0).unwrap().is_ascii_digit() {
-                mappings[idx-1].push(&line);
-            }
-        } else {
-            idx += 1;
+            previous = new_previous;
         }
     }
-    // Sort ranges for binary search
-    seeds.sort();
-    soil2seed.sort();
-    fert2soil.sort();
-    water2fert.sort();
-    light2water.sort();
-    temp2light.sort();
-    hum2temp.sort();
-    loc2hum.sort();
-    // Traverse mappings
-    // println!("{:?}", seeds);
-    let max = 10000000000;
-    for loc in 1..max {
-        let hum = loc2hum.map(loc);
-        // println!("{}, {}", loc, hum);
-        let temp = hum2temp.map(hum);
-        // println!("{}, {}", hum, temp);
-        let light = temp2light.map(temp);
-        // println!("{}, {}", temp, light);
-        let water = light2water.map(light);
-        // println!("{}, {}", light, water);
-        let fert = water2fert.map(water);
-        // println!("{}, {}", water, fert);
-        let soil = fert2soil.map(fert);
-        // println!("{}, {}", fert, soil);
-        let seed = soil2seed.map(soil);
-        // println!("{}, {}", soil, seed);
-        let seed_range = seeds.find_interval(seed);
-        // println!("seed {}", seed);
-        if seed_range.src_start != seed_range.src_end {
-            println!("{}", loc);
-            break;
-        }
-    }
-    // Find minimum
-    // println!("{:?}", seed2soil);
-    // println!("{:?}", soil2fert);
-    // println!("{:?}", fert2water);
-    // println!("{:?}", water2light);
-    // println!("{:?}", light2temp);
-    // println!("{:?}", temp2hum);
-    // println!("{:?}", hum2loc);
+    previous.append(&mut current);
+    // println!("We got {:?} in the end", previous);
+    let res = previous.iter().map(|(a, _)| a).reduce(|a, b| std::cmp::min(a, b)).unwrap().to_string();
+    println!("{}", res);
     Ok(())
 }
