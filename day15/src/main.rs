78597b58477e2a9cee3e3fbddbad378d9680f2c1
Jerry-Master <joseperez2000@hotmail.es>
2023-12-15 17:38:39 +0100

    part2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -29,19 +29,64 @@ fn main() -> io::Result<()>{
             .map(|x| String::from(x))
             .collect::<Vec<String>>());
     }
-    let mut res = 0;
+    let mut boxes: Vec<Vec<(String, u32)>> = vec![Default::default();256];
     for step in steps {
-        res += hash(&step);
+        let tmp_hash = hash(&step);
+        update(&mut boxes, tmp_hash, &step);
+    }
+    let mut res = 0;
+    for (i, caja) in boxes.into_iter().enumerate() {
+        for (j, element) in caja.into_iter().enumerate() {
+            res += (i + 1) * (j + 1) * element.1 as usize;
+        }
     }
     println!("{}", res);
     Ok(())
 }
 
 
-fn hash(seq: &String) -> u32 {
+fn update(boxes: &mut Vec<Vec<(String, u32)>>, idx: usize, step: &String) {
+    let name = step.split('-').nth(0).unwrap().split('=').nth(0).unwrap();
+    if step.chars().last().unwrap() == '-' {
+        delete(&mut boxes[idx], name);
+    } else {
+        let focal = step.chars().last().unwrap().to_digit(10).unwrap();
+        insert_or_update(&mut boxes[idx], name, focal);
+    }
+}
+
+
+fn delete(caja: &mut Vec<(String, u32)>, name: &str) {
+    let mut aux = vec![];
+    for element in caja.iter() {
+        if element.0 != name {
+            aux.push(element.clone());
+        }
+    }
+    *caja = aux;
+}
+
+
+fn insert_or_update(caja: &mut Vec<(String, u32)>, name: &str, focal: u32) {
+    let mut is_inside = false;
+    for element in caja.iter_mut() {
+        if element.0 == name {
+            element.1 = focal;
+            is_inside = true;
+        }
+    }
+    if !is_inside {
+        caja.push((String::from(name), focal));
+    }
+}
+
+
+
+fn hash(seq: &String) -> usize {
     let mut res = 0;
     for char in seq.chars() {
-        res += (char as u8) as u32;
+        if char == '-' || char == '=' { break; }
+        res += (char as u8) as usize;
         res *= 17;
         res %= 256;
     }
