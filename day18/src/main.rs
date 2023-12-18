aa9a6f5e64133968223b7e66c22ac0542cf9cbe0
Jerry-Master <joseperez2000@hotmail.es>
2023-12-18 21:44:16 +0100

    part2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use clap::Parser;
-use std::cmp::{max, min};
+use std::cmp::{max, min, Ord, Ordering};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None, help_template = "\
@@ -18,7 +18,7 @@ struct Args {
 }
 
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
 enum Dir {
     Up,
     Down,
@@ -26,11 +26,39 @@ enum Dir {
     Right,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Instr {
     dir: Dir,
     length: usize,
-    color: [u8; 3]
+}
+
+#[derive(Eq, PartialEq, Clone, Copy, Debug)]
+enum CornerType {
+    UL,
+    UR,
+    DL,
+    DR
+}
+
+#[derive(Eq, PartialEq, Clone, Copy, Debug)]
+struct Corner {
+    ctype: CornerType,
+    i: usize,
+    j: usize
+}
+
+
+impl Ord for Corner {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.i.cmp(&other.i).then_with(|| self.j.cmp(&other.j))
+    }
+}
+
+
+impl PartialOrd for Corner {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 
@@ -43,116 +71,194 @@ fn main() -> io::Result<()>{
     for line in reader.lines() {
         let line = line?;
         let mut words = line.split_whitespace();
+        let (hex_dir, hex_length) = decode_hex(words.nth(2).unwrap());
         instructs.push(
             Instr {
-                dir: match words.next().unwrap().chars().nth(0).unwrap() {
+                dir: match hex_dir {
                     'L' => Dir::Left,
                     'R' => Dir::Right,
                     'U' => Dir::Up,
                     'D' => Dir::Down,
                     _ => panic!(),
                 },
-                length: words.next().unwrap().parse().unwrap(),
-                color: parse_color(words.next().unwrap()),
+                length: hex_length,
             }
         )
     }
-    // println!("{:#?}", instructs);
-    let ((m, n), start) = get_shape(&instructs);  // Board of m x n
-    println!("{}, {}", m, n);
-    let mut board: Vec<Vec<char>> = vec![vec!['.'; n]; m];
-    draw(&mut board, &instructs, start);
-    fill(&mut board);
-    for row in &board {
-        println!("{}", row.iter().collect::<String>());
-    }
-    let res = count(&board);
+    let start = get_shape(&instructs);  // Board of m x n
+    let mut nodes = Vec::<Corner>::new();
+    read_nodes(&mut nodes, &instructs, start);
+    nodes.sort();
+    let mut buckets: Vec<Vec<Corner>> = vec![];
+    fill_buckets(&mut buckets, &nodes);
+    let res = get_area(&buckets);
     println!("{}", res);
     Ok(())
 }
 
 
-fn fill(board: &mut Vec<Vec<char>>) {
-    let mut copy = board.clone();
-    for i in 0..board.len() {
-        let mut is_in = false;
-        let mut first_dir: Dir = Dir::Right;
-        let mut last_dir: Dir = Dir::Right;
-        for j in 0..(board[0].len() - 1) {
-            if board[i][j] == '#' {
-                if j == 0 || board[i][j - 1] == '.' {  // First encounter
-                    if i > 0 && board[i - 1][j] == '#' {
-                        first_dir = Dir::Up;
-                    } 
-                    if i < board.len() - 1 && board[i + 1][j] == '#' {
-                        if first_dir == Dir::Up {
-                            first_dir = Dir::Right;
-                        } else {
-                            first_dir = Dir::Down;
-                        }
-                    }
-                }
-                if board[i][j + 1] == '.' {  // Last encounter
-                    if i > 0 && board[i - 1][j] == '#' {
-                        last_dir = Dir::Up;
-                    } 
-                    if i < board.len() - 1 && board[i + 1][j] == '#' {
-                        if last_dir == Dir::Up {
-                            last_dir = Dir::Right;
-                        } else {
-                            last_dir = Dir::Down;
-                        }
-                    }
-                    if first_dir != last_dir || last_dir == Dir::Right {
-                        is_in = !is_in;
-                    }
-                    (first_dir, last_dir) = (Dir::Right, Dir::Right);
-                }
-            } else if is_in {
-                copy[i][j] = '#';
-            }
+fn decode_hex(hex: &str) -> (char, usize) {
+    let length = usize::from_str_radix(&hex[2..3], 16).unwrap() * 65536
+        + usize::from_str_radix(&hex[3..5], 16).unwrap() * 256
+        + usize::from_str_radix(&hex[5..7], 16).unwrap();
+    let dir = match hex.chars().nth(7).unwrap() {
+            '0' => 'R',
+            '1' => 'D',
+            '2' => 'L',
+            '3' => 'U',
+            _ => panic!(),
+        };
+    (dir, length)
+}
+
+
+fn get_area(buckets: &Vec<Vec<Corner>>) -> usize {
+    let mut last_bucket = buckets[0].clone();
+    let mut total = sum_interval_lengths(&last_bucket);
+    for k in 1..buckets.len() {
+        let bucket = &buckets[k];
+        total += get_area_from_buckets(bucket, &last_bucket);
+        update_bucket(&mut last_bucket, bucket);
+    }
+    total
+}
+
+
+fn update_bucket(last_bucket: &mut Vec<Corner>, bucket: &Vec<Corner>) {
+    for element in last_bucket.iter_mut() {
+        element.i = bucket[0].i;
+    }
+    last_bucket.append(&mut bucket.clone());
+    last_bucket.sort();
+    // Remove duplicates
+    let mut aux_bucket = Vec::<Corner>::new();
+    for k in 0..last_bucket.len() {
+        if k > 0 && k < last_bucket.len() - 1 && last_bucket[k].j != last_bucket[k-1].j && last_bucket[k].j != last_bucket[k+1].j {
+            aux_bucket.push(last_bucket[k]);
+        } else if k == 0 && last_bucket[k].j != last_bucket[k+1].j {
+            aux_bucket.push(last_bucket[k]);
+        } else if k == last_bucket.len() - 1 && last_bucket[k].j != last_bucket[k-1].j {
+            aux_bucket.push(last_bucket[k]);
         }
     }
-    *board = copy;
+    *last_bucket = aux_bucket.clone();
 }
 
 
-fn count(board: &Vec<Vec<char>>) -> u32 {
-    board
-        .iter()
-        .map(
-            |x| 
-                x
-                .iter()
-                .filter(|&&x| x == '#')
-                .count() as u32
-        ).sum()
+fn get_area_from_buckets(b1: &Vec<Corner>, b2: &Vec<Corner>) -> usize {
+    let dist = b1[0].i - b2[0].i;
+    let length = sum_interval_lengths(b2);
+    let extra = sum_extra_length(b1, b2);
+    dist * length + extra
 }
 
 
-fn draw(board: &mut Vec<Vec<char>>, instructs: &Vec<Instr>, start: (usize, usize)) {
-    let (mut i, mut j): (usize, usize) = start;
-    for instr in instructs {
-        for d in 0..instr.length {
-            match instr.dir {
-                Dir::Up => board[i - d][j] = '#',
-                Dir::Down => board[i + d][j] = '#',
-                Dir::Left => board[i][j - d] = '#',
-                Dir::Right => board[i][j + d] = '#',
-            };
-            (i, j) = (i, j);
+fn sum_extra_length(b1: &Vec<Corner>, b2: &Vec<Corner>) -> usize {
+    assert!(b1.len() % 2 == 0);
+    let mut total = 0;
+    let mut is_in = false;
+    let mut k2 = 0;
+    for k in 0..b1.len() / 2 {
+        while k2 < b2.len() && b2[k2].j < b1[2 * k + 1].j {
+            if b2[k2].j != b1[2 * k].j {
+                is_in = !is_in;
+            }
+            k2 += 1;
         }
+        if k2 < b2.len() && b2[k2].j == b1[2 * k + 1].j { k2 += 1; }
+        if (b1[2 * k].ctype == CornerType::DR && b1[2 * k + 1].ctype == CornerType::UL && !is_in) ||
+         (b1[2 * k].ctype == CornerType::UR && b1[2 * k + 1].ctype == CornerType::DL && is_in) {
+            total += b1[2 * k + 1].j - b1[2 * k].j;
+        }
+        if b1[2 * k].ctype == CornerType::DR && b1[2 * k + 1].ctype == CornerType::DL && !is_in {
+            total += b1[2 * k + 1].j - b1[2 * k].j + 1;
+        }
+        if b1[2 * k].ctype == CornerType::UR && b1[2 * k + 1].ctype == CornerType::UL && is_in {
+            total += b1[2 * k + 1].j - b1[2 * k].j - 1;
+        }
+        if (b1[2 * k].ctype == CornerType::DR && b1[2 * k + 1].ctype == CornerType::UL) ||
+         (b1[2 * k].ctype == CornerType::UR && b1[2 * k + 1].ctype == CornerType::DL) {
+            is_in = !is_in;
+        }
+    }
+    total
+}
+
+
+fn sum_interval_lengths(b: &Vec<Corner>) -> usize {  // Intervals come in pairs of nodes
+    assert!(b.len() % 2 == 0);
+    let mut total = 0;
+    for k in 0..b.len() / 2 {
+        total += b[2 * k + 1].j - b[2 * k].j + 1;
+    }
+    total
+}
+
+
+fn fill_buckets(buckets: &mut Vec<Vec<Corner>>, nodes: &Vec<Corner>) {
+    buckets.push(vec![nodes[0]; 1]);
+    let mut prev_i = nodes[0].i;
+    for k in 1..nodes.len() {
+        let node = nodes[k];
+        if prev_i != node.i {
+            buckets.push(vec![node; 1]);
+        } else {
+            let n = buckets.len();
+            buckets[n - 1].push(node);
+        }
+        prev_i = node.i;
+    }
+}
+
+
+fn read_nodes(nodes: &mut Vec<Corner>, instructs: &Vec<Instr>, start: (usize, usize)) {
+    let (mut i, mut j) = start;
+    let mut last_dir = instructs[0].dir;
+    let first_dir = instructs[0].dir;
+    match instructs[0].dir {
+        Dir::Left => j -= instructs[0].length,
+        Dir::Right => j += instructs[0].length,
+        Dir::Down => i += instructs[0].length,
+        Dir::Up => i -= instructs[0].length,
+    };
+    for k in 1..instructs.len() {
+        let instr = instructs[k];
+        nodes.push(Corner { ctype: match (last_dir, instr.dir) {
+            (Dir::Left, Dir::Up) => CornerType::UR,
+            (Dir::Left, Dir::Down) => CornerType::DR,
+            (Dir::Right, Dir::Up) => CornerType::UL,
+            (Dir::Right, Dir::Down) => CornerType::DL,
+            (Dir::Up, Dir::Left) => CornerType::DL,
+            (Dir::Down, Dir::Left) => CornerType::UL,
+            (Dir::Up, Dir::Right) => CornerType::DR,
+            (Dir::Down, Dir::Right) => CornerType::UR,
+            (_, _) => panic!(),
+        }, i: i, j: j });
+        last_dir = instr.dir;
         match instr.dir {
-            Dir::Up => i = i - instr.length,
-            Dir::Down => i = i + instr.length,
-            Dir::Left => j = j - instr.length,
-            Dir::Right => j = j + instr.length,
+            Dir::Left => j -= instr.length,
+            Dir::Right => j += instr.length,
+            Dir::Down => i += instr.length,
+            Dir::Up => i -= instr.length,
         };
     }
+    if first_dir != last_dir {
+        nodes.push(Corner { ctype: match (last_dir, first_dir) {
+            (Dir::Left, Dir::Up) => CornerType::UR,
+            (Dir::Left, Dir::Down) => CornerType::DR,
+            (Dir::Right, Dir::Up) => CornerType::UL,
+            (Dir::Right, Dir::Down) => CornerType::DL,
+            (Dir::Up, Dir::Left) => CornerType::DL,
+            (Dir::Down, Dir::Left) => CornerType::UL,
+            (Dir::Up, Dir::Right) => CornerType::DR,
+            (Dir::Down, Dir::Right) => CornerType::UR,
+            (_, _) => panic!(),
+        }, i: i, j: j });
+    }
 }
 
 
-fn get_shape(instructs: &Vec<Instr>) -> ((usize, usize), (usize, usize)){
+fn get_shape(instructs: &Vec<Instr>) -> (usize, usize) {
     let (mut m, mut n) = (1, 1);
     let (mut m_max, mut n_max) = (1, 1);
     let (mut m_min, mut n_min) = (1, 1);
@@ -164,17 +270,5 @@ fn get_shape(instructs: &Vec<Instr>) -> ((usize, usize), (usize, usize)){
             Dir::Up => { m -= instr.length as i32; m_min = min(m, m_min) },
         }
     }
-    (
-        ((m_max - m_min + 1) as usize, (n_max - n_min + 1) as usize),
-        ((1 - m_min) as usize, (1 - n_min) as usize)
-    )
-}
-
-
-fn parse_color(hex: &str) -> [u8; 3] {
-    [
-        u8::from_str_radix(&hex[2..4], 16).unwrap(),
-        u8::from_str_radix(&hex[4..6], 16).unwrap(),
-        u8::from_str_radix(&hex[6..8], 16).unwrap(),    
-    ]
+    ((1 - m_min) as usize, (1 - n_min) as usize)
 }
