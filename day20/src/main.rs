e34e55400a74d18dbbf9c98d51c5c5dfc5361ac9
Jerry-Master <joseperez2000@hotmail.es>
2023-12-20 09:46:18 +0100

    part2

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,4 +1,3 @@
-use core::num;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use clap::Parser;
@@ -102,21 +101,47 @@ fn main() -> io::Result<()>{
     // println!("{:#?}", tmp_graph);
     let mut graph = HashMap::<String, (NodeType, Vec<String>)>::new();
     construct_graph(tmp_graph, &mut graph);
-    let (mut total_low, mut total_high) = (0, 0);
-    for _ in 0..1000 {
-        let (num_low, num_high) = simulate(&mut graph);
-        total_low += num_low; total_high += num_high;
+    let (mut nd_count, mut pc_count, mut vd_count, mut tx_count) = (0, 0, 0, 0);
+    let (mut nd_first, mut pc_first, mut vd_first, mut tx_first) = (true, true, true, true);
+    let update = |x: &mut usize, y: &mut bool, c: usize| {if *x > 0 && *y {
+        *y = false;
+        *x = c - *x;
+    } else if *y {
+        *x = c - *x;
+    }};
+    for count in 1..10000 {
+        let (nd_activated, pc_activated, vd_activated, tx_activated) = simulate(&mut graph);
+        if nd_activated {
+            update(&mut nd_count, &mut nd_first, count);
+        }
+        if pc_activated {
+            update(&mut pc_count, &mut pc_first, count);
+        }
+        if vd_activated {
+            update(&mut vd_count, &mut vd_first, count);
+        }
+        if tx_activated {
+            update(&mut tx_count, &mut tx_first, count);
+        }
     }
-    println!("{}", total_high * total_low);
+    println!("nd count: {}", nd_count);
+    println!("pc count: {}", pc_count);
+    println!("vd count: {}", vd_count);
+    println!("tx count: {}", tx_count);
+    println!("rx count: {}", mcm(&vec![nd_count, pc_count, vd_count, tx_count]));
     Ok(())
 }
 
 
-fn simulate(graph: &mut HashMap<String, (NodeType, Vec<String>)>) -> (usize, usize) {
+fn simulate(graph: &mut HashMap<String, (NodeType, Vec<String>)>) -> (bool, bool, bool, bool) {
     let mut queue = VecDeque::<(String, Pulse, String)>::new();  // left sends pulse to right
-    queue.push_back((String::from(""), Pulse::Low, String::from("broadcaster"))); 
-    let (mut n_low, mut n_high) = (1, 0);
+    queue.push_back((String::from(""), Pulse::Low, String::from("broadcaster")));
+    let (mut nd_activated, mut pc_activated, mut vd_activated, mut tx_activated) = (false, false, false, false); 
     while let Some(transmission) = queue.pop_front() {
+        if transmission.1 == Pulse::High && transmission.0 == "nd" { nd_activated = true; }
+        if transmission.1 == Pulse::High && transmission.0 == "pc" { pc_activated = true; }
+        if transmission.1 == Pulse::High && transmission.0 == "vd" { vd_activated = true; }
+        if transmission.1 == Pulse::High && transmission.0 == "tx" { tx_activated = true; }
         let new_pulse_opt = match &mut graph.entry(transmission.2.clone()).or_insert((NodeType::Broadcaster, vec![])).0 {
             NodeType::FlipFlop(f) => f.propagate(transmission.1),
             NodeType::Conjunction(f) => Some(f.propagate(transmission.1, transmission.0)),
@@ -125,14 +150,10 @@ fn simulate(graph: &mut HashMap<String, (NodeType, Vec<String>)>) -> (usize, usi
         if let Some(new_pulse) = new_pulse_opt {
             for child in graph[&transmission.2].1.iter() {
                 queue.push_back((transmission.2.clone(), new_pulse, child.clone()));
-                match new_pulse {
-                    Pulse::High => n_high += 1,
-                    Pulse::Low => n_low += 1,
-                }
             }
         }
     }
-    (n_low, n_high)
+    (nd_activated, pc_activated, vd_activated, tx_activated)
 }
 
 
@@ -164,4 +185,23 @@ fn get_parents(tmp_graph: &HashMap<String, Vec<String>>) -> HashMap<String, Hash
         }
     }
     res
+}
+
+fn mcm(vec: &Vec<usize>) -> usize {
+    let mut mcm = vec[0];
+    for i in 1..vec.len() {
+        mcm = mcm_binary(mcm, vec[i]);
+    }
+    return mcm;
+}
+
+fn mcm_binary(a: usize, b: usize) -> usize {
+    return a * b / mcd_binary(a, b);
+}
+
+fn mcd_binary(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    return mcd_binary(b, a % b);
 }
