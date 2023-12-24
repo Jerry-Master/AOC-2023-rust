4bb25a99a6ea9728e66c83e9f5f8d41a52e9ed50
Jerry-Master <joseperez2000@hotmail.es>
2023-12-24 18:01:49 +0100

    part2

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use clap::Parser;
 use std::cmp::max;
+use std::collections::{VecDeque, HashMap};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None, help_template = "\
@@ -45,13 +46,99 @@ fn main() -> io::Result<()>{
                 .next() {
                     Some((i, _)) => i as isize,
                     None => panic!(),
-                };
-    let mut dp: Vec<Vec<Option<u32>>> = vec![vec![None; map[0].len()]; map.len()];
-    // println!("{:?}, {:?}", start, end);
-    let mut vis = vec![vec![false; map[0].len()]; map.len()];
-    let res = compute_maximum(&map, &mut dp, &mut vis, (0, start.unwrap()), (map.len() as isize - 1, end));
+                };    
+    let graph = construct_graph(&map, (0, start.unwrap()), (map.len() as isize - 1, end));
+    // println!("{:?}", graph);
+    let mut vis = vec![false; graph.len()];
+    vis[0] = true;
+    let mut res = 0;
+    brute_force_count(&graph, &mut vis, 0, 1, 0, &mut res);
     println!("{}", res);
     Ok(())
+}
+
+
+fn brute_force_count(graph: &Vec<Vec<(usize, u32)>>, vis: &mut Vec<bool>, start: usize, end: usize, dist: u32, res: &mut u32) {
+    if start == end { 
+        *res = max(*res, dist);
+    }
+    for &(node, next_dist) in graph[start].iter() {
+        if vis[node] { continue; }
+        vis[node] = true;
+        brute_force_count(graph, vis, node, end, dist + next_dist, res);
+        vis[node] = false;
+    }
+}
+
+
+fn construct_graph(map: &Vec<Vec<char>>, start: (isize, isize), end: (isize, isize)) -> Vec<Vec<(usize, u32)>> {
+    // Find intersections
+    let mut graph = vec![];
+    let mut nodes = vec![];
+    let mut nodes_dict = HashMap::<(isize, isize), usize>::new();
+    nodes.push((start.0 as usize, start.1 as usize));
+    nodes.push((end.0 as usize, end.1 as usize));
+    for i in 1..map.len() - 1 {
+        for j in 1..map[0].len() - 1 {
+            if check_intersection(map, (i,j)) {
+                nodes_dict.insert((i as isize, j as isize), nodes.len());
+                nodes.push((i, j));
+            }
+        }
+    }
+    // println!("{:?}", nodes);
+    // BFS from each intersection to the others
+    for (i, node) in nodes.iter().enumerate() {
+        let mut queue = VecDeque::<((usize, usize), u32)>::new();
+        queue.push_back((*node, 0));
+        let mut vis = vec![vec![false; map[0].len()]; map.len()];
+        while let Some((pos, dist)) = queue.pop_front() {
+            if vis[pos.0][pos.1] { continue; }
+            vis[pos.0][pos.1] = true;
+            for dir in [Dir::Up, Dir::Down, Dir::Left, Dir::Right] {
+                let next = advance((pos.0 as isize, pos.1 as isize), dir);
+                if !out_of_bounds(map, next) && map[next.0 as usize][next.1 as usize] != '#' {
+                    if check_intersection(map, (next.0 as usize, next.1 as usize)) && !vis[next.0 as usize][next.1 as usize] {
+                        if graph.len() <= i {
+                            graph.push(vec![]);
+                        } 
+                        graph[i].push((nodes_dict[&next], dist + 1));
+                    } else {
+                        queue.push_front(((next.0 as usize, next.1 as usize), dist + 1));
+                    }
+                }
+            }
+        }
+    }
+    // Make undirected graph
+    for i in 0..graph.len() {
+        let mut adj = vec![];
+        for &(j, dist) in graph[i].iter() {
+            adj.push((j, dist));
+        }
+        'outer: for (j, dist) in adj {
+            for node in graph[j].iter() {
+                if node.0 == i {
+                    continue 'outer;
+                }
+            }
+            graph[j].push((i, dist));
+        }
+    }
+    graph
+}
+
+
+fn check_intersection(map: &Vec<Vec<char>>, pos: (usize, usize)) -> bool {
+    if map[pos.0][pos.1] == '#' { return false; }
+    let mut count = 0;
+    for dir in [Dir::Up, Dir::Down, Dir::Left, Dir::Right] {
+        let next_pos = advance((pos.0 as isize, pos.1 as isize), dir);
+        if !out_of_bounds(map, next_pos) && map[next_pos.0 as usize][next_pos.1 as usize] != '#' {
+            count += 1;
+        }
+    }
+    count > 2
 }
 
 
@@ -60,41 +147,6 @@ enum Dir {
     Down,
     Left,
     Right,
-}
-
-
-fn compute_maximum(map: &Vec<Vec<char>>, dp: &mut Vec<Vec<Option<u32>>>, vis: &mut Vec<Vec<bool>>, start: (isize, isize), end: (isize, isize)) -> u32 {
-    if start == end { return 0; }
-    if let Some(res) = dp[end.0 as usize][end.1 as usize] { return res; }
-    vis[end.0 as usize][end.1 as usize] = true;
-    // println!("{:?}", end);
-    let mut res = 0;
-    let next = advance(end, Dir::Up);
-    if !out_of_bounds(map, next) && !vis[next.0 as usize][next.1 as usize] &&
-        (map[next.0 as usize][next.1 as usize] == '.' || map[next.0 as usize][next.1 as usize] == 'v') {
-        res = max(res, compute_maximum(map, dp, vis, start, next) + 1);
-    }
-
-    let next = advance(end, Dir::Down);
-    if !out_of_bounds(map, next) && !vis[next.0 as usize][next.1 as usize] &&
-        (map[next.0 as usize][next.1 as usize] == '.' || map[next.0 as usize][next.1 as usize] == '^') {
-        res = max(res, compute_maximum(map, dp, vis, start, next) + 1);
-    }
-
-    let next = advance(end, Dir::Left);
-    if !out_of_bounds(map, next) && !vis[next.0 as usize][next.1 as usize] &&
-        (map[next.0 as usize][next.1 as usize] == '.' || map[next.0 as usize][next.1 as usize] == '>') {
-        res = max(res, compute_maximum(map, dp, vis, start, next) + 1);
-    }
-
-    let next = advance(end, Dir::Right);
-    if !out_of_bounds(map, next) && !vis[next.0 as usize][next.1 as usize] &&
-        (map[next.0 as usize][next.1 as usize] == '.' || map[next.0 as usize][next.1 as usize] == '<') {
-        res = max(res, compute_maximum(map, dp, vis, start, next) + 1);
-    }
-    dp[end.0 as usize][end.1 as usize] = Some(res);
-    vis[end.0 as usize][end.1 as usize] = false;
-    res
 }
 
 
