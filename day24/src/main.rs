a21711bc0e03942f58adbe75a41caab906a2d6df
Jerry-Master <joseperez2000@hotmail.es>
2023-12-24 20:38:59 +0100

    part2

--- day24/src/main.rs
+++ day24/src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use clap::Parser;
+use z3::ast::{Ast, Int};
+use z3::{Config, Context, Solver};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None, help_template = "\
@@ -19,12 +21,12 @@ struct Args {
 
 #[derive(Clone, Copy, Debug)]
 struct Hail {
-    px: i128,
-    py: i128,
-    pz: i128,
-    vx: i128,
-    vy: i128,
-    vz: i128,
+    px: i64,
+    py: i64,
+    pz: i64,
+    vx: i64,
+    vy: i64,
+    vz: i64,
 }
 
 
@@ -60,14 +62,46 @@ fn main() -> io::Result<()>{
     for line in reader.lines() {
         hails.push(parse(line?));
     }
-    let mut res = 0;
-    for i in 0..hails.len() { 
-        for j in i+1..hails.len() {
-            if Hail::intersect_2d(hails[i], hails[j]) { res += 1; }
-        }
-    }
-    println!("{}", res);
+    solve(&hails);
     Ok(())
+}
+
+
+// Z3 logic from arthomnix's part2: https://github.com/arthomnix/aoc23/blob/master/src/days/day24.rs
+// Code from: https://github.com/tymscar/Advent-Of-Code/blob/master/2023/rust/src/day24/part2.rs
+fn solve(hails: &Vec<Hail>) {
+    let cfg = Config::new();
+    let ctx = Context::new(&cfg);
+    let solver = Solver::new(&ctx);
+
+    let px = Int::new_const(&ctx, "px");
+    let py = Int::new_const(&ctx, "py");
+    let pz = Int::new_const(&ctx, "pz");
+    let vx = Int::new_const(&ctx, "vx");
+    let vy = Int::new_const(&ctx, "vy");
+    let vz = Int::new_const(&ctx, "vz");
+
+    for hailstone in hails {
+        let pxn = Int::from_i64(&ctx, hailstone.px);
+        let pyn = Int::from_i64(&ctx, hailstone.py);
+        let pzn = Int::from_i64(&ctx, hailstone.pz);
+        let vxn = Int::from_i64(&ctx, hailstone.vx);
+        let vyn = Int::from_i64(&ctx, hailstone.vy);
+        let vzn = Int::from_i64(&ctx, hailstone.vz);
+        let tn = Int::fresh_const(&ctx, "t");
+
+        solver.assert(&(&pxn + &vxn * &tn)._eq(&(&px + &vx * &tn)));
+        solver.assert(&(&pyn + &vyn * &tn)._eq(&(&py + &vy * &tn)));
+        solver.assert(&(&pzn + &vzn * &tn)._eq(&(&pz + &vz * &tn)));
+    }
+
+    solver.check();
+    let model = solver.get_model().unwrap();
+    let x = model.get_const_interp(&px).unwrap().as_i64().unwrap();
+    let y = model.get_const_interp(&py).unwrap().as_i64().unwrap();
+    let z = model.get_const_interp(&pz).unwrap().as_i64().unwrap();
+
+    println!("{}", x + y + z);
 }
 
 
